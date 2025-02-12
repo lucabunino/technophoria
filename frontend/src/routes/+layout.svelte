d16b06e29c1a946d6ff13cfea1a914c02b52aa1a
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-02-12 10:32:11 +0100

    Alts

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -91,12 +91,12 @@ function toggleCredits() {
   
 <svelte:head>
   <title>Technophoria</title>
-  <meta name="description" content="To be done">
+  <meta name="description" content="An ode to the power of music and the spaces it creates">
   <link rel="canonical" href={$page.url}>
   <meta name="robots" content="index,follow">
   <meta name="googlebot" content="index,follow">
   <meta property="og:title" content="Technophoria">
-  <meta property="og:description" content="To be done">
+  <meta property="og:description" content="An ode to the power of music and the spaces it creates">
   <meta property="og:url" content={$page.url}>
   <meta property="og:type" content="website">
   <meta property="og:site_name" content="Technophoria">
